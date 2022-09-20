5bc7f6b4c2ff77d9bb8cd5e6e733a6fd0c05690f
Dariya Popova <dariyp26@mail.ru>
2022-09-20 20:05:50 +0300

    ДЗ к семинару 9

---
+++ task66/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+void SummNaturNumb(int M, int N)
+{
+  int summ = 0;
+  Console.Write("Cуммa натуральных элементов в промежутке от M до N: ");
+  if (M < 1) M = 1;
+  while (M <= N)
+  {
+    summ += M;
+    M++;
+  }
+  Console.WriteLine(summ);
+}
+
+Console.WriteLine("Задайте значения M и N.");
+Console.Write("M = ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("N = ");
+int N = int.Parse(Console.ReadLine());
+SummNaturNumb(M, N);
---
+++ task68/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 3, n = 2 -> A(m,n) = 29 */
+
+int Akkerman(int M, int N)
+{
+  if (M == 0)
+    return N + 1;
+  else
+    if ((M != 0) && (N == 0))
+    return Akkerman(M - 1, 1);
+  else
+    return Akkerman(M - 1, Akkerman(M, N - 1));
+}
+
+Console.WriteLine("Вычисление функции Аккермана с помощью рекурсии -> A(m,n).");
+Console.WriteLine("Задайте значения m и n.");
+Console.Write("m = ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("n = ");
+int N = int.Parse(Console.ReadLine());
+Console.Write("A(m,n) = " + Akkerman(M, N));
